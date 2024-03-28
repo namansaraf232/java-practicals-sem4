624de3c4c0aff1403ac9c3f3cdc728fd53b0f283
namansaraf232 <162601630+namansaraf232@users.noreply.github.com>
2024-03-28 11:50:15 +0530

    sem 4 java prac

---
+++ Program16_AdderClass.java
@@ -0,0 +1,25 @@
+//Write a java program to add two numbers using class and object.
+// Date : 03/01/2024, Author : Yash Wadhvani
+
+import java.util.*;
+
+class adder {
+    int add(int a, int b) {
+        return (a + b);
+    }
+}
+
+public class Program16_AdderClass {
+    public static void main(String[] args) {
+        int n1, n2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number 1 :-");
+        n1 = sc.nextInt();
+        System.out.println("Enter Number 2 :-");
+        n2 = sc.nextInt();
+        sc.close();
+
+        adder a = new adder();
+        System.out.println(a.add(n1, n2));
+    }
+}
